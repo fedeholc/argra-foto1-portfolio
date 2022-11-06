0b719e6409441fc962429eeb08210f691c632053
fedeholc <federicoholc@gmail.com>
2022-11-06 18:34:18 -0300

    feat poner titulos de secciones

--- src/trabajosPracticos.js
+++ src/trabajosPracticos.js
@@ -1,26 +1,39 @@
 import NavBar from "./navbar";
 import Footer from "./footer";
+import FotoConEpigrafe from "./fotoConEpigrafe";
 
 const TrabajosPracticos = () => {
   return (
     <>
       <NavBar />
-      <h2>Trabajos Practicos</h2>
-      <div className="imagen-container">
-        <img
-          className="imagen-portfolio"
-          alt="poner epigrafe"
-          src="https://images4.imagebam.com/56/59/ed/MEGCXE8_o.jpg"
-        />
+      <div class="div-flex-derecha">
+        <h1 class="seccion-titulo">Trabajos Prácticos</h1>
       </div>
-      <hr></hr>
-      <div className="imagen-container">
-        <img
-          className="imagen-portfolio"
-          alt="poner epigrafe"
-          src="https://images4.imagebam.com/56/59/ed/MEGCXE8_o.jpg"
-        />
+      <div class="div-flex-centrado">
+        <h2 class="seccion-subtitulo">24 de Marzo</h2>
       </div>
+
+      <FotoConEpigrafe
+        src="https://images4.imagebam.com/56/59/ed/MEGCXE8_o.jpg"
+        epigrafe="hola"
+      />
+
+      <div class="div-flex-centrado">
+        <h2 class="seccion-subtitulo">Deportes</h2>
+      </div>
+
+      <FotoConEpigrafe
+        src="https://images4.imagebam.com/56/59/ed/MEGCXE8_o.jpg"
+        epigrafe="hola"
+      />
+
+      <h2>Espectáculos</h2>
+
+      <FotoConEpigrafe
+        src="https://images4.imagebam.com/56/59/ed/MEGCXE8_o.jpg"
+        epigrafe="hola"
+      />
+
       <Footer />
     </>
   );
